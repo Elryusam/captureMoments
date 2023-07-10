7eafb4631a0d189927a46429c0f1ce0a19910afe
Elryusam <138589857+Elryusam@users.noreply.github.com>
2023-07-09 20:22:48 -0500

    prueba

--- server.js
+++ server.js
@@ -36,18 +36,42 @@ app.listen(port, () => {
 });
 
 // Agrega la lógica para interactuar con la base de datos dentro de tus rutas
-app.get('/api/obtener-datos', (req, res) => {
-  const request = new Request('SELECT * FROM TableName', (err, rowCount, rows) => {
-    if (err) {
-      console.error('Error al ejecutar la consulta:', err);
-      res.status(500).json({ error: 'Error al obtener datos de la base de datos' });
-    } else {
-      const data = rows.map(row => row.reduce((acc, column) => {
-        acc[column.metadata.colName] = column.value;
-        return acc;
-      }, {}));
-      res.json(data);
-    }
+app.get('/api/entradas', (req, res) => {
+    const query = 'SELECT * FROM entradas_blog';
+    const request = new Request(query, (err, rowCount, rows) => {
+      if (err) {
+        console.error('Error al ejecutar la consulta:', err);
+        res.status(500).json({ error: 'Error al obtener datos de la base de datos' });
+      } else {
+        const data = rows.map(row => ({
+          id: row[0].value,
+          titulo: row[1].value,
+          contenido: row[2].value,
+          fecha_publicacion: row[3].value,
+          autor: row[4].value,
+          url_imagen: row[5].value
+        }));
+        res.json(data);
+      }
+    });
+    connection.execSql(request);
   });
-  connection.execSql(request);
-});
+  
+  app.get('/api/comentarios', (req, res) => {
+    const query = 'SELECT * FROM comentarios';
+    const request = new Request(query, (err, rowCount, rows) => {
+      if (err) {
+        console.error('Error al ejecutar la consulta:', err);
+        res.status(500).json({ error: 'Error al obtener datos de la base de datos' });
+      } else {
+        const data = rows.map(row => ({
+          id: row[0].value,
+          contenido: row[1].value,
+          fecha_publicacion: row[2].value,
+          id_entrada: row[3].value
+        }));
+        res.json(data);
+      }
+    });
+    connection.execSql(request);
+  });
