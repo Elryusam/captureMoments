74bb8fce0819646a1cd53336d0c52dcbab3b6fa0
Elryusam <138589857+Elryusam@users.noreply.github.com>
2023-07-12 20:25:42 -0500

    final changes

--- js/formulario.js
+++ js/formulario.js
@@ -1,21 +1,21 @@
 function validarFormulario() {
-    var nombre = document.getElementById('nombre').value;
-    var email = document.getElementById('email').value;
-    var telefono = document.getElementById('telefono').value;
-    var mensaje = document.getElementById('mensaje').value;
-    
+    const nombre = document.getElementById('nombre').value;
+    const email = document.getElementById('email').value;
+    const telefono = document.getElementById('telefono').value;
+    const mensaje = document.getElementById('mensaje').value;
+
     if (nombre === '' || email === '' || telefono === '' || mensaje === '') {
         alert('Por favor, complete todos los campos del formulario.');
         return false;
     }
     
-    var formatoCorreo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const formatoCorreo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!formatoCorreo.test(email)) {
         alert('Por favor, ingrese un correo electrónico válido.');
         return false;
     }
     
-    var formatoTelefono = /^\d+$/;
+    const formatoTelefono = /^\d+$/;
     if (!formatoTelefono.test(telefono) || telefono.length < 10) {
         alert('Por favor, ingrese un número de teléfono válido (10 numeros).');
         return false;
